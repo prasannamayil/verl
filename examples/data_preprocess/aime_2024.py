740f403d160b597bf669cebf2f8787094e3be7e6
prasannamayil <prasanna.mayilvahanan@gmail.com>
2025-03-12 16:56:51 +0100

    added data preprocessing of curated thoughts, gpqa_diamond, aime_2024

---
+++ examples/data_preprocess/aime_2024.py
@@ -0,0 +1,101 @@
+# Copyright 2024 Bytedance Ltd. and/or its affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Preprocess the AIME 2024 dataset to parquet format
+"""
+
+import os
+import datasets
+
+from verl.utils.hdfs_io import copy, makedirs
+import argparse
+
+from verl.utils.reward_score.math import remove_boxed, last_boxed_only_string
+
+
+def extract_solution(solution_str):
+    return remove_boxed(last_boxed_only_string(solution_str))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--local_dir', default='~/data/aime_2024')
+    parser.add_argument('--hdfs_dir', default=None)
+
+    args = parser.parse_args()
+
+    data_source = 'HuggingFaceH4/aime_2024'
+    print(f"Loading the {data_source} dataset from huggingface...", flush=True)
+    dataset = datasets.load_dataset(data_source)
+
+    # AIME 2024 only has a train split, but we'll use it as our test set
+    test_dataset = dataset['train']
+    print(f"Loaded dataset with {len(test_dataset)} examples")
+
+    instruction_following = "Let's think step by step and output the final answer within \\boxed{}."
+
+    # Process function for each data item
+    def make_map_fn(split):
+        def process_fn(example, idx):
+            problem = example.pop('problem')
+            question = problem + ' ' + instruction_following
+
+            answer = example.pop('answer')
+            solution = example.pop('solution', None)
+            year = example.pop('year', None)
+            problem_id = example.pop('id', None)
+            url = example.pop('url', None)
+            
+            data = {
+                "data_source": data_source,
+                "prompt": [{
+                    "role": "user",
+                    "content": question
+                }],
+                "ability": "math",
+                "reward_model": {
+                    "style": "rule",
+                    "ground_truth": answer
+                },
+                "extra_info": {
+                    'split': split,
+                    'index': idx,
+                    'id': problem_id,
+                    'year': year,
+                    'url': url,
+                    'solution': solution
+                }
+            }
+            return data
+
+        return process_fn
+
+    # Process the dataset
+    test_dataset = test_dataset.map(function=make_map_fn('test'), with_indices=True)
+    print(f"Processed {len(test_dataset)} examples")
+
+    local_dir = args.local_dir
+    hdfs_dir = args.hdfs_dir
+
+    # Create directory if it doesn't exist
+    os.makedirs(os.path.expanduser(local_dir), exist_ok=True)
+    
+    # Save test dataset
+    test_dataset.to_parquet(os.path.join(local_dir, 'test.parquet'))
+    print(f"Saved test dataset with {len(test_dataset)} examples to {os.path.join(local_dir, 'test.parquet')}")
+
+    if hdfs_dir is not None:
+        makedirs(hdfs_dir)
+        copy(src=local_dir, dst=hdfs_dir)
+        print(f"Copied data to HDFS: {hdfs_dir}") 
